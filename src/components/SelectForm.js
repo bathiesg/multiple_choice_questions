4022c68a71181bcfb54aa7dad713f7c33c81580e
Samba Gueye <gueye.samba@gmail.com>
2023-01-29 23:00:15 +0000

    Refactoring

--- src/components/SelectForm.js
+++ src/components/SelectForm.js
@@ -1,30 +1,51 @@
 import { useState } from "react";
 
-const SelectForm = ({options, refName, setSelectedOption, isComplete, answer }) => {
-    const [value, setValue] = useState(answer)  
+const SelectForm = ({
+  options,
+  refName,
+  setSelectedOption,
+  isComplete,
+  answer,
+}) => {
+  const [value, setValue] = useState(answer);
 
-    const handleChange = (event) => {   
-        const index = event.target.selectedIndex;
-        
-        const el = event.target.childNodes[index]
-        console.log(event.target)
-        const optionid =  el.getAttribute('optionid');  
-        setValue(event.target.value); 
-       
-        setSelectedOption({
-            optionId: optionid,
-            answer: event.target.value,
-        }) 
-    }   
-   
-      return (
-        <label>
-            <select name={refName} value={value} onChange={handleChange} disabled={isComplete} className="form-input">  
-                <option  optionid="0" key="0" value="" disabled>Choose an option</option>
-                {options.map(option => <option  optionid={option.id} key={option.id} value={option.value || answer}>{option.value}</option>)}          
-            </select>
-        </label>
-      );
-}
+  const handleChange = (event) => {
+    const index = event.target.selectedIndex;
 
-export default SelectForm
+    const el = event.target.childNodes[index];
+    const optionid = el.getAttribute("optionid");
+    setValue(event.target.value);
+
+    setSelectedOption({
+      optionId: optionid,
+      answer: event.target.value,
+    });
+  };
+
+  return (
+    <label>
+      <select
+        name={refName}
+        value={value}
+        onChange={handleChange}
+        disabled={isComplete}
+        className="form-input"
+      >
+        <option optionid="0" key="0" value="" disabled>
+          Choose an option
+        </option>
+        {options.map((option) => (
+          <option
+            optionid={option.id}
+            key={option.id}
+            value={option.value || answer}
+          >
+            {option.value}
+          </option>
+        ))}
+      </select>
+    </label>
+  );
+};
+
+export default SelectForm;
